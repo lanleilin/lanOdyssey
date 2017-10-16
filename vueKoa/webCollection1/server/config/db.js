83da4c41c5e53e511f302d41a839af314ab77b29
lanleilin <2476081528@qq.com>
2017-10-16 14:28:12 +0800

    modify psw
    Change-Id: I137ed9378d92bdf6ffe1fabd02cac718cffb4d59

--- vueKoa/webCollection1/server/config/db.js
+++ vueKoa/webCollection1/server/config/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 // 使用 url 形式连接数据库
-const theDb = new Sequelize('mysql://root:password@localhost/my_test_db', {
+const theDb = new Sequelize('mysql://root:admin@localhost/my_test_db', {
     define: {
         timestamps: false // 取消Sequelzie自动给数据表添加的 createdAt 和 updatedAt 两个时间戳字段
     }
